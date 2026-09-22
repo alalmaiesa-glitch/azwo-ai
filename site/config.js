409a28dad2ecb46cbdc07454cbcdf6e813454606
alalmaiesa-glitch <alalmaiesa@gmail.com>
2026-09-22 18:16:41 +0300

    feat: connect website-v1 to live Supabase project

--- site/config.js
+++ site/config.js
@@ -1,5 +1,5 @@
 window.AZWO_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  demoMode: true
+  supabaseUrl: "https://kywffsqebvjoyuswxtiz.supabase.co",
+  supabaseAnonKey: "sb_publishable_vNFWU3vMDfb04KxO80DHVA_N4I5UPTx",
+  demoMode: false
 };
